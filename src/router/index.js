032919c61463c80b1f959989c3be6953a3324394
glissario <cgliss2000@gmail.com>
2021-09-18 10:49:56 +0200

    fixed: fixes WebHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const HomeGot = () => import("../views/HomeGot.vue");
 const FamilyList = () => import("../views/FamilyList.vue");
@@ -29,7 +29,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
